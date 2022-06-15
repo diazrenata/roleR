f5833fee2ce36f7be3019adc496b86bc51119815
ajrominger <ajrominger@gmail.com>
2022-06-15 10:18:26 -0400

    wrote real working example of use

--- inst/example_use.R
+++ inst/example_use.R
@@ -1,12 +1,30 @@
 library(roleR)
 
-# make params
-p <- roleParams()
+# make one set of params
+params1 <- roleParams(individuals_local = 100, individuals_meta = 1000,
+                      species_meta = 500, speciation_local = 0.1,
+                      speciation_meta = 1, extinction_meta = 0.8,
+                      trait_sigma = 1, env_sigma = 1,
+                      comp_sigma = 1, dispersal_prob = 0.1, mutation_rate = 0.01,
+                      equilib_escape = 1, num_basepairs = 250,
+                      init_type = 'oceanic_island', niter = 100,
+                      niterTimestep = 50)
 
-# make arguments
-argz <- roleArguments(niter, ..., params = p)
+# make another slightly different set
+params2 <- params1
+slot(params2, 'init_type') <- 'bridge_island'
 
-# pass arguments to roleExperiment
-e <- roleExperiment(argz)
+# make one model run
+oneMod <- roleModel(params1)
+
+# make an experiment with multiple models
+oneExp <- roleExperiment(list(params1, params2))
+
+# check out the metadata
+oneExp@experimentMeta
+
+# get the current state for one model run
+
+# get the timeseries of all states for one model run
 
 
