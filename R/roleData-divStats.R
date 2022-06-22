a6fbf5caf652748b194e7fecf0110e0562ba59a6
ajrominger <ajrominger@gmail.com>
2022-06-22 13:04:30 -0400

    refactored hill stats to have unique function for each data type

--- R/roleData-divStats.R
+++ R/roleData-divStats.R
@@ -3,8 +3,8 @@
 #'     indicates what type of hill number each function caculates; e.g. 
 #'     `hillAbund` calcualtes abundance-based Hill numbers, while `hillTrait` 
 #'     calculates trait-based Hill numbers
-#' @param x the `roleData` object to calculate Hill numbers from
-#' @param q the Hill number exponent 
+#' @param x the `roleData` object to calculate Hill numbers or richness from
+#' @param q the Hill number exponents
 #' @param ... additional parameters, ignored
 #' 
 #' 
@@ -12,43 +12,23 @@
 #' @export
 
 # abundance hill stats
+#' @rdname div-sumStats
+#' @export
+
 setGeneric('hillAbund', 
            def = function(x, q, ...) standardGeneric('hillAbund'), 
            signature = 'x')
 
 
-setMethod('hillStats', 
+setMethod('hillAbund', 
           signature = 'roleData', 
-          definition = function(x, q, type) {
-              type <- match.arg(type, 
-                                c('abundance', 'trait', 'genetic', 'phylo'))
+          definition = function(x, q) {
+              X <- rawAbundance(x)[[1]]
+              X <- X[X > 0]
               
-              if(type == 'abundance') {
-                  X <- rawAbundance(x)[[1]]
-                  X <- X[X > 0]
-                  
-                  f <- .hillDivSAD
-              } else if(type == 'trait') {
-                  spp <- rawSppID(x)[[1]]
-                  trt <- rawTraits(x)[[1]]
-                  
-                  X <- lapply(unique(spp), function(i) {
-                      xs <- sum(spp == i)
-                      xt <- mean(trt[spp == i])
-                      
-                      return(c(xs, xt))
-                  })
-                  X <- do.call(rbind, X)
-                  
-                  f <- .hillDivTrait
-              }
-              
-              return(f(X, q))
+              return(.hillDivSAD(X, q))
           }
 )
-
-
-
 
 
 # function for abundance-based
@@ -66,15 +46,51 @@ setMethod('hillStats',
 
 
 # genetic hill stats
+#' @rdname div-sumStats
+#' @export
+
 setGeneric('hillGenetic', 
            def = function(x, q, ...) standardGeneric('hillGenetic'), 
            signature = 'x')
 
 
+setMethod('hillGenetic', 
+          signature = 'roleData', 
+          definition = function(x, q) {
+              X <- rawGenDiv(x)[[1]]
+              X <- X[!is.na(X)]
+              
+              return(.hillDivSAD(X, q))
+          }
+)
+
+
 # trait hill stats
+#' @rdname div-sumStats
+#' @export
+
 setGeneric('hillTrait', 
            def = function(x, q, ...) standardGeneric('hillTrait'), 
            signature = 'x')
+
+
+setMethod('hillTrait', 
+          signature = 'roleData', 
+          definition = function(x, q) {
+              spp <- rawSppID(x)[[1]]
+              trt <- rawTraits(x)[[1]]
+              
+              X <- lapply(unique(spp), function(i) {
+                  xs <- sum(spp == i)
+                  xt <- mean(trt[spp == i])
+                  
+                  return(c(xs, xt))
+              })
+              X <- do.call(rbind, X)
+              
+              return(.hillDivTrait(X, q))
+          }
+)
 
 
 # function for trait-based
@@ -102,6 +118,23 @@ setGeneric('hillTrait',
     
     return(D)
 }
+
+
+# phylo hill (place holder)
+#' @rdname div-sumStats
+#' @export
+
+setGeneric('hillPhylo', 
+           def = function(x, q, ...) standardGeneric('hillPhylo'), 
+           signature = 'x')
+
+
+setMethod('hillPhylo', 
+          signature = 'roleData', 
+          definition = function(x, q) {
+              return(q)
+          }
+)
 
 
 
